9c42e10b02bf014e5acdc9d06474f14158436c49
Joseph <poedude5229@gmail.com>
2024-02-21 07:18:10 -0500

    Default Text

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,10 @@ const cors = require("cors");
 const csurf = require("csurf");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const routes = require("./routes");
 
 const { environment } = require("./config");
+const { isPort } = require("validator");
 const isProduction = environment === "production";
 
 const app = express();
@@ -20,3 +22,23 @@ if (!isProduction) {
 }
 
 //helmet sets a variety of headers to provide app security
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+//Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(routes);
+
+module.exports = app;
---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+// ...
+
+module.exports = router;
