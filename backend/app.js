df2eac28cd246d8c88b1b739f78b3ccbf6749ed2
Joseph <poedude5229@gmail.com>
2024-02-21 21:04:26 -0500

    Default Text

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const routes = require("./routes");
 
+const { ValidationError } = require("sequelize");
 const { environment } = require("./config");
 const { isPort } = require("validator");
 const isProduction = environment === "production";
@@ -40,5 +41,39 @@ app.use(
 );
 
 app.use(routes);
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
 
+
+// ...
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = "Validation error";
+    err.errors = errors;
+  }
+  next(err);
+});
+
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 module.exports = app;
