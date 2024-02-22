5675cbf22c29256f59880619a5be801505eb1b59
Joseph <poedude5229@gmail.com>
2024-02-21 21:26:13 -0500

    Schema made in migrations 1

---
+++ backend/db/migrations/20240222021720-create-user.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const { process_params } = require('express/lib/router');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA // defines the schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      hashedPassword: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+      },
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    // await queryInterface.dropTable("Users");
+
+    options.tableName = "Users";
+    await queryInterface.dropTable(options);
+
+  },
+};
