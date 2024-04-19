aa2108b9b49e44ab3969ddf21f248a2ff1ec66a3
Joseph <poedude5229@gmail.com>
2024-04-19 16:09:03 -0400

    Method 2 Redux setup

---
+++ frontend/src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import configureStore from "./store";
+
+const store = configureStore();
+
+if (import.meta.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
