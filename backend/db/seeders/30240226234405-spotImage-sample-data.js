ed3a70139974f85738b92f2d71b4ece52f51084c
Joseph <poedude5229@gmail.com>
2024-04-27 12:14:45 -0400

    Fixed the callout box

--- backend/db/seeders/30240226234405-spotImage-sample-data.js
+++ backend/db/seeders/30240226234405-spotImage-sample-data.js
@@ -30,13 +30,43 @@ module.exports = {
           preview: false,
         },
         {
+          spotId: 1,
+          url: "https://photos.zillowstatic.com/fp/b595414cacc9042e2d20e55de96f52a1-uncropped_scaled_within_1536_1152.webp",
+          preview: false,
+        },
+        {
+          spotId: 1,
+          url: "https://photos.zillowstatic.com/fp/e08f4569033679d827ea9eb4ccfddcaf-uncropped_scaled_within_1536_1152.webp",
+          preview: false,
+        },
+        {
           spotId: 2,
           url: "https://photos.zillowstatic.com/fp/1d3932fa7386493e3b213cfcd8ee57da-uncropped_scaled_within_1536_1152.webp",
           preview: true,
         },
         {
+          spotId: 2,
+          url: "https://photos.zillowstatic.com/fp/9a4dcf35efa06b5edb97a21e575d814d-uncropped_scaled_within_1536_1152.webp",
+          preview: false,
+        },
+        {
+          spotId: 2,
+          url: "https://photos.zillowstatic.com/fp/d2b61817ca35a853ee8d86e82b605d6d-uncropped_scaled_within_1536_1152.webp",
+          preview: false,
+        },
+        {
+          spotId: 2,
+          url: "https://photos.zillowstatic.com/fp/b261ff8e004914ea9a2f15b5874b5c51-uncropped_scaled_within_1536_1152.webp",
+          preview: false,
+        },
+        {
           spotId: 3,
           url: "https://ap.rdcpix.com/97597954d01ad109654da6b7aeb3b8dal-m1419331428od-w1024_h768_x2.webp?w=750&q=75",
+          preview: true,
+        },
+        {
+          spotId: 3,
+          url: "",
           preview: true,
         },
         {
--- frontend/src/components/Spot/Spot.jsx
+++ frontend/src/components/Spot/Spot.jsx
@@ -7,6 +7,7 @@ import { deleteReviewThunk, spotReviews } from "../../store/reviews";
 import "../../../public/assets/index-167b04b0.css";
 import MakeReview from "../Reviews/MakeReview";
 import "./modal.css";
+import "./Spot.css";
 import { FaStar } from "react-icons/fa";
 function DetailsPage() {
   let { spotId } = useParams();
@@ -18,8 +19,8 @@ function DetailsPage() {
   }, [dispatch, toggleOne]);
 
   const closeModal = () => {
-    setShowModal(false);
     setToggleOne(true);
+    setShowModal(false);
   };
 
   let spot = useSelector((state) => state.spots);
@@ -28,6 +29,7 @@ function DetailsPage() {
   let rv = Object.values(reviews);
   // console.log("Reviews: " + rv);
   let reversedReviews = rv.reverse();
+  reversedReviews;
   // console.log("reversedReviews: " + reversedReviews);
   const handleDelete = async () => {
     const reviewToDelete = rv.find(
@@ -58,7 +60,10 @@ function DetailsPage() {
 
   const [showModal, setShowModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const toggleModal = () => setShowModal(!showModal);
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    setToggleOne(!toggleOne);
+  };
   const toggleDeleteModal = () => setShowDeleteModal(!showDeleteModal);
   const months = [
     "January",
@@ -84,140 +89,193 @@ function DetailsPage() {
               {selected.city}, {selected.state} {selected.country}
             </h3>
           </header>
-          <div className="spotDetailsImages">
+          <div
+            className="spotDetailsImages"
+            style={
+              {
+                // display: "flex",
+                // flexDirection: "row",
+                // flexWrap: "wrap",
+                // gap: "24px",
+                // width: "1000px",
+                // height: "500px",
+              }
+            }
+          >
             <img
+              // style={{ width: "700px", height: "500px" }}
               src={selected?.SpotImages?.[0]?.url}
               alt={`Spot ${spotId} preview image`}
               className="largeThumbnail"
             />
-            <div className="smallImagesContainer">
+            <div
+              className="smallImagesContainer"
+              style={
+                {
+                  // display: "flex",
+                  // flexDirection: "row",
+                  // maxHeight: "500px",
+                  // height: "500px",
+                  // width: "300px",
+                  // maxWidth: "300px",
+                }
+              }
+            >
               <img
-                src={selected.SpotImages?.[1]?.url}
+                style={{ width: "300px", height: "220px" }}
+                src={
+                  selected.SpotImages?.[1]?.url ||
+                  "https://res.cloudinary.com/djuzk5um3/image/upload/v1710993252/am-bnb%20authme_Project/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available_ykibfw.webp"
+                }
                 alt="small spot picture one"
               />
               <img
-                src={selected.SpotImages?.[2]?.url}
+                style={{ width: "300px", height: "220px" }}
+                src={
+                  selected.SpotImages?.[2]?.url ||
+                  "https://res.cloudinary.com/djuzk5um3/image/upload/v1710993252/am-bnb%20authme_Project/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available_ykibfw.webp"
+                }
                 alt="small spot picture 2"
               />
               <img
-                src={selected.SpotImages?.[3]?.url}
+                style={{ width: "300px", height: "220px" }}
+                src={
+                  selected.SpotImages?.[3]?.url ||
+                  "https://res.cloudinary.com/djuzk5um3/image/upload/v1710993252/am-bnb%20authme_Project/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available_ykibfw.webp"
+                }
                 alt="small spot picture 3"
               />
               <img
-                src={selected.SpotImages?.[4]?.url}
+                style={{ width: "300px", height: "220px" }}
+                src={
+                  selected.SpotImages?.[4]?.url ||
+                  "https://res.cloudinary.com/djuzk5um3/image/upload/v1710993252/am-bnb%20authme_Project/depositphotos_244011872-stock-illustration-image-vector-symbol-missing-available_ykibfw.webp"
+                }
                 alt="small spot picture 4"
               />
             </div>
-            <div className="spotInfo">
-              <div className="description">
-                <h3>{`Hosted by ${selected.Owner?.firstName} ${selected.Owner?.lastName}`}</h3>
-                <p>{selected?.description}</p>
-                <span className="spotPrice">{`$${selected?.price} / night`}</span>
-                <span className="spotStars">
-                  {!existing &&
-                    sessionUser &&
-                    sessionUser.id !== selected.ownerId && (
-                      <button onClick={toggleModal}>
-                        {rv.length > 0
-                          ? "Post Your Review!"
-                          : "Be the first to leave a review!"}
-                      </button>
-                    )}
-                  <div className="reviewBoard">
-                    {sessionUser && sessionUser?.id !== selected.ownerId && (
-                      <button
-                        onClick={() => window.alert("Feature coming soon...")}
-                      >
-                        Reserve
-                      </button>
-                    )}
-                    <div
-                      className="reviewHeader"
-                      style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        gap: "5px",
-                      }}
+          </div>
+          <div className="spotInfo">
+            <div className="description">
+              <h3>{`Hosted by ${selected.Owner?.firstName} ${selected.Owner?.lastName}`}</h3>
+              <p>{selected?.description}</p>
+              <span className="spotPrice">{`$${selected?.price} / night`}</span>
+              <br />
+              <span className="spotStars">
+                {!existing &&
+                  sessionUser &&
+                  sessionUser.id !== selected.ownerId && (
+                    <button onClick={toggleModal}>
+                      {rv.length > 0
+                        ? "Post Your Review!"
+                        : "Be the first to leave a review!"}
+                    </button>
+                  )}
+                <div style={{ position: "absolute", right: "12px" }}>
+                  <h3>{ selected.price}</h3>
+                  {sessionUser && sessionUser?.id !== selected.ownerId && (
+                    <button
+                      onClick={() => window.alert("Feature coming soon...")}
                     >
-                      <h3>
-                        <FaStar style={{ color: "#EAAA00" }} />
+                      Reserve
+                    </button>
+                  )}
+                </div>
+                <div className="reviewBoard">
+                  <div
+                    className="reviewHeader"
+                    style={{
+                      display: "flex",
+                      flexDirection: "row",
+                      gap: "5px",
+                    }}
+                  >
+                    <h3>
+                      <FaStar style={{ color: "#EAAA00" }} />
 
-                        {rv.length > 0
-                          ? average % 1 !== 0
-                            ? `${average} stars · `
-                            : `${stringAvg} stars · `
-                          : ""}
-                      </h3>
+                      {rv.length > 0
+                        ? average % 1 !== 0
+                          ? `${average} stars · `
+                          : `${stringAvg} stars · `
+                        : ""}
+                    </h3>
 
-                      <h3>
-                        {rv.length === 0
-                          ? "New"
-                          : rv.length > 1
-                          ? `${rv.length} reviews`
-                          : `1 review`}
-                        {/* {rv.length > 1
+                    <h3>
+                      {rv.length === 0
+                        ? "New"
+                        : rv.length > 1
+                        ? `${rv.length} reviews`
+                        : `1 review`}
+                      {/* {rv.length > 1
                           ? ` ${rv.length} reviews`
                           : ` ${rv.length} review`} */}
-                      </h3>
-                    </div>
-                    <ul style={{ listStyle: "none" }}>
-                      {rv.length === 0 ? "Be the first to leave a review!" : rv.map((review) => (
-                        <li key={review.id}>
-                          <h4>
-                            {review?.User?.firstName +
-                              `, ${
-                                months[review.createdAt?.slice(0, 1) - 1]
-                              } ${review.createdAt?.slice(5, 9)}`}
-                          </h4>
-                          <p>
-                            <FaStar />
-                            <b> {review.stars} stars</b>
-                          </p>
-                          <p>{review.review}</p>
-                          {sessionUser && sessionUser.id === review.userId && (
-                            <button onClick={toggleDeleteModal}>Delete</button>
-                          )}
-                          {showDeleteModal && (
-                            <div className="modal" onClick={toggleDeleteModal}>
-                              <div
-                                className="modal-content"
-                                onClick={(e) => e.stopPropagation()}
-                              >
-                                <span
-                                  className="close"
-                                  style={{ cursor: "pointer" }}
-                                  onClick={toggleDeleteModal}
-                                >
-                                  X
-                                </span>
-                                <h2>Confirm Delete</h2>
-                                <p>
-                                  Are you sure you want to delete this review?
-                                </p>
-                                <button
-                                  style={{
-                                    backgroundColor: "grey",
-                                    color: "White",
-                                  }}
-                                  onClick={toggleDeleteModal}
-                                >{`No (Keep Review)`}</button>
-                                <button
-                                  style={{ backgroundColor: "red" }}
-                                  onClick={() => {
-                                    handleDelete(); // console.log(review.id);
-                                  }}
-                                >
-                                  {`Yes (Delete Review)`}
-                                </button>
-                              </div>
-                            </div>
-                          )}
-                        </li>
-                      ))}
-                    </ul>
+                    </h3>
                   </div>
-                </span>
-              </div>
+                  <ul style={{ listStyle: "none" }}>
+                    {rv.length === 0
+                      ? "Be the first to leave a review!"
+                      : rv.map((review) => (
+                          <li key={review.id}>
+                            <h4>
+                              {review?.User?.firstName +
+                                `, ${
+                                  months[review.createdAt?.slice(0, 1) - 1]
+                                } ${review.createdAt?.slice(5, 9)}`}
+                            </h4>
+                            <p>
+                              <FaStar />
+                              <b> {review.stars} stars</b>
+                            </p>
+                            <p>{review.review}</p>
+                            {sessionUser &&
+                              sessionUser.id === review.userId && (
+                                <button onClick={toggleDeleteModal}>
+                                  Delete
+                                </button>
+                              )}
+                            {showDeleteModal && (
+                              <div
+                                className="modal"
+                                onClick={toggleDeleteModal}
+                              >
+                                <div
+                                  className="modal-content"
+                                  onClick={(e) => e.stopPropagation()}
+                                >
+                                  <span
+                                    className="close"
+                                    style={{ cursor: "pointer" }}
+                                    onClick={toggleDeleteModal}
+                                  >
+                                    X
+                                  </span>
+                                  <h2>Confirm Delete</h2>
+                                  <p>
+                                    Are you sure you want to delete this review?
+                                  </p>
+                                  <button
+                                    style={{
+                                      backgroundColor: "grey",
+                                      color: "White",
+                                    }}
+                                    onClick={toggleDeleteModal}
+                                  >{`No (Keep Review)`}</button>
+                                  <button
+                                    style={{ backgroundColor: "red" }}
+                                    onClick={() => {
+                                      handleDelete(); // console.log(review.id);
+                                    }}
+                                  >
+                                    {`Yes (Delete Review)`}
+                                  </button>
+                                </div>
+                              </div>
+                            )}
+                          </li>
+                        ))}
+                  </ul>
+                </div>
+              </span>
             </div>
           </div>
         </div>
