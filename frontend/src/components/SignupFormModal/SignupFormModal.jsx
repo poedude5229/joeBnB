9ede2272874db509daba4f06f5ece4bfc92b57e0
Joseph <poedude5229@gmail.com>
2024-04-28 02:24:00 -0400

    Styled signup

--- frontend/src/components/SignupFormModal/SignupFormModal.jsx
+++ frontend/src/components/SignupFormModal/SignupFormModal.jsx
@@ -58,74 +58,92 @@ function SignupFormModal() {
   }, [email, username, firstName, lastName, password, confirmPassword]);
 
   return (
-    <>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
+    <div>
+      <h1 style={{textAlign: "center"}}>Sign Up</h1>
+      <form onSubmit={handleSubmit} style={{display: "flex", flexDirection: "column", width: "290px"}}>
         <label>
           Email
+          <br />
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
+            placeholder="Email"
+            style={{width: "260px", marginLeft: "5px"}}
           />
         </label>
         {errors.email && <p>{errors.email}</p>}
         <label>
           Username
+          <br />
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
-            required
+          required
+          placeholder="Username"
+          style={{width: "260px", marginLeft: "5px"}}
           />
         </label>
         {errors.username && <p>{errors.username}</p>}
         <label>
           First Name
+          <br />
           <input
             type="text"
             value={firstName}
             onChange={(e) => setFirstName(e.target.value)}
             required
+            style={{ width: "260px", marginLeft: "5px" }}
+            placeholder="First Name"
           />
         </label>
         {errors.firstName && <p>{errors.firstName}</p>}
         <label>
           Last Name
+          <br />
           <input
             type="text"
             value={lastName}
             onChange={(e) => setLastName(e.target.value)}
             required
+            style={{ width: "260px", marginLeft: "5px" }}
+            placeholder="Last Name"
           />
         </label>
         {errors.lastName && <p>{errors.lastName}</p>}
         <label>
           Password
+          <br />
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            style={{ width: "260px", marginLeft: "5px" }}
+            placeholder="Password"
           />
         </label>
         {errors.password && <p>{errors.password}</p>}
         <label>
           Confirm Password
+          <br />
           <input
             type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
+            style={{ width: "260px", marginLeft: "5px" }}
+            placeholder="Confirm Password"
           />
         </label>
         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
-        <button type="submit" disabled={block}>
+        <button style={{backgroundColor: "#EAAA00", width: "260px", height: "30px", color: "darkblue", alignSelf: "center", marginTop: "5px", marginBottom: "5px",  }} type="submit" disabled={block}>
           Sign Up
         </button>
       </form>
-    </>
+    </div>
   );
 }
 
